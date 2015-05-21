166fd9c504f9aefbfa718b594a9892425e46b9f7
kevin_000 <kevin_000@SamsungUltra.wireless.hhs.nl>
2015-05-21 14:36:49 +0200

    Probeerseltjes. Probleem erkent: Kan geen associaties inladen.

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,14 +1,35 @@
 <?php
-	namespace App\Model\Entity;
-	
-	use Cake\Auth\DefaultPasswordHasher;
-	use Cake\ORM\Entity;
-	
-	class User extends Entity {
-	    protected $_accessible = ['*' => true];
-	
-	    protected function _setPassword($password) {
-	        return (new DefaultPasswordHasher)->hash($password);
-	    }
-	}
+
+namespace App\Model\Entity;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\Entity;
+
+class User extends Entity {
+
+    protected $_accessible = ['*' => true];
+
+    protected function _setPassword($password) {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
+
+    public $hasMany = array(
+        'Interest' => array(
+            'className' => 'Interest',
+            'foreignKey' => 'userID' ///check
+        ),
+        'Skill' => array(
+            'className' => 'Skill',
+            'foreignKey' => 'userID',
+        ),
+        'Project' => array(
+            'className' => 'Project',
+            'foreignKey' => 'userID',
+        ),
+        'SocialLink' => array(
+            'className' => 'SocialLink',
+            'foreignKey' => 'userID',
+        )
+    );
+}
 ?>
--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -1,42 +1,43 @@
 <?php
-	namespace App\Model\Table;
-	
-	use Cake\ORM\Table;
-	use Cake\Validation\Validator;
-	
-	class UsersTable extends Table {
-		
-		/**
-		 * Vastleggen van de associaties
-		 */
-		public function initialize(array $config) {
-			$this->hasMany('Interests', [
-				'foreignKey' => 'userID'
-			]);
-			
-			$this->hasMany('SocialLinks', [
-				'foreignKey' => 'userID'
-			]);
-			
-			$this->hasMany('Projects', [
-				'foreignKey' => 'userID'
-			]);
-			
-			$this->hasMany('Skills', [
-				'foreignKey' => 'userID'
-			]);
-		}
-		
-		/**
-		 * Valideren van velden
-		 */
-	    public function validationDefault(Validator $validator) {
-	        return $validator
-				->notEmpty('firstname', 'A first name is required')
-				->notEmpty('lastname', 'A last name is required')
-				->notEmpty('username', 'A username is required')
-				->notEmpty('username', 'A username is required');
-	    }
-	
-	}
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class UsersTable extends Table {
+
+    /**
+     * Vastleggen van de associaties
+     */
+    public function initialize(array $config) {
+        $this->hasMany('Interests', [
+            'foreignKey' => 'userID'
+        ]);
+
+        $this->hasMany('SocialLinks', [
+            'foreignKey' => 'userID'
+        ]);
+
+        $this->hasMany('Projects', [
+            'foreignKey' => 'userID'
+        ]);
+
+        $this->hasMany('Skills', [
+            'foreignKey' => 'userID'
+        ]);
+    }
+
+    /**
+     * Valideren van velden
+     */
+    public function validationDefault(Validator $validator) {
+        return $validator
+                        ->notEmpty('firstname', 'A first name is required')
+                        ->notEmpty('lastname', 'A last name is required')
+                        ->notEmpty('username', 'A username is required')
+                        ->notEmpty('username', 'A username is required');
+    }
+
+}
 ?>
